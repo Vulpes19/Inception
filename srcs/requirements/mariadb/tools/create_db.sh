8f2648d80961978f1d8339bc4f017fc9fe73553b
Vulpes19 <baioumyaymann@gmail.com>
2023-04-26 15:52:36 +0100

    fixed all mariadb container errors

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -6,7 +6,7 @@
 #    By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/14 14:37:42 by abaioumy          #+#    #+#              #
-#    Updated: 2023/04/26 13:17:17 by abaioumy         ###   ########.fr        #
+#    Updated: 2023/04/26 14:51:13 by abaioumy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,18 +24,7 @@ mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON WordPress.* TO 'strang
 mysql -u root -e "CREATE USER 'Ayman'@'%' IDENTIFIED BY 'Vulpes19';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'Ayman'@'%' WITH GRANT OPTION;"
 
-#restructuring database
 mysql -u root WordPress << EOF
-DELIMITER $$
-CREATE FUNCTION is_valid_username(username VARCHAR(20)) RETURNS BOOLEAN
-BEGIN
-    DECLARE is_valid BOOLEAN;
-    IF ( username LIKE '%admin%' OR username LIKE '%Admin%' OR username LIKE '%administrator%' OR username LIKE '%Administrator' ) THEN
-        SET is_valid = FALSE;
-    else
-        SET is_valid = TRUE;
-    END IF;
-    RETURN is_valid;
-END $$
-DELIMITER ;
-EOF
+echo "Database created"
+service mysql stop
+mysqld
