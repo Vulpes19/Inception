5021b005ccd8bb8ed2ee362c61e43ebcb89fd8fb
Vulpes19 <baioumyaymann@gmail.com>
2023-04-26 15:03:21 +0100

    fixed some syntax problems, but still problem connecting to sockets

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -6,17 +6,18 @@
 #    By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/14 14:37:42 by abaioumy          #+#    #+#              #
-#    Updated: 2023/04/14 15:39:08 by abaioumy         ###   ########.fr        #
+#    Updated: 2023/04/26 13:17:17 by abaioumy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
+service mysql start
 #creating database
 mysql -u root -e "CREATE DATABASE WordPress;"
 
 #creating user
-mysql -u root -e "CREATE USER 'stranger'@% IDENTIFIED BY 'reallygoodpassword';"
+mysql -u root -e "CREATE USER 'stranger'@'%' IDENTIFIED BY 'reallygoodpassword';"
 mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE ON WordPress.* TO 'stranger'@'%';"
 
 #creating admin user
@@ -25,6 +26,7 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'Ayman'@'%' WITH GRANT OPTION;"
 
 #restructuring database
 mysql -u root WordPress << EOF
+DELIMITER $$
 CREATE FUNCTION is_valid_username(username VARCHAR(20)) RETURNS BOOLEAN
 BEGIN
     DECLARE is_valid BOOLEAN;
@@ -35,4 +37,5 @@ BEGIN
     END IF;
     RETURN is_valid;
 END $$
+DELIMITER ;
 EOF
