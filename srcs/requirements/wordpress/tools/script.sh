fb33e92ccb5c1522d07f0795c2b7db48143b56a7
Vulpes19 <baioumyaymann@gmail.com>
2023-05-03 14:13:43 +0100

    docker compose is working

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,7 +6,7 @@
 #    By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/27 14:08:41 by abaioumy          #+#    #+#              #
-#    Updated: 2023/05/01 14:21:18 by abaioumy         ###   ########.fr        #
+#    Updated: 2023/05/03 11:58:46 by abaioumy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,5 +16,5 @@
 cd /var/www/html/wordpress/
 wp core download --allow-root --force
 wp config create --dbname=wordpress --dbuser=ayman --dbpass=vulpes19 --dbhost=mariadb --allow-root
-# wp core install --url=localhost:9000 --title=inception --admin_user=abaioumy --admin_password=Baioumy123 --allow-root
+wp core install --url=localhost:9000 --title=inception --admin_email=baioumyaymann@gmail.com --admin_user=abaioumy --admin_password=Baioumy123 --allow-root
 exec "$@"
