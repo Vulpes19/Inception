b91c05e38da2feeba03d1d4c57b442c2c3b6f158
Vulpes19 <baioumyaymann@gmail.com>
2023-05-11 14:45:26 +0100

    added env variables but for some reason it shows the forbidden error

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -6,17 +6,16 @@
 #    By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/14 14:37:42 by abaioumy          #+#    #+#              #
-#    Updated: 2023/04/30 18:05:37 by abaioumy         ###   ########.fr        #
+#    Updated: 2023/05/11 14:13:29 by abaioumy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 service mysql start
-#creating database
 echo "CREATE DATABASE wordpress;" >> data.sql 
 
-echo "CREATE USER 'ayman'@'%' IDENTIFIED BY 'vulpes19';" >> data.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'ayman'@'%' WITH GRANT OPTION;" >> data.sql
+echo "CREATE USER '$(MYSQL_USER)'@'%' IDENTIFIED BY '$(MYSQL_PASSWORD)';" >> data.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$(MYSQL_USER)'@'%' WITH GRANT OPTION;" >> data.sql
 
 mysql < data.sql
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,7 +6,7 @@
 #    By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/27 14:08:41 by abaioumy          #+#    #+#              #
-#    Updated: 2023/05/11 11:58:29 by abaioumy         ###   ########.fr        #
+#    Updated: 2023/05/11 14:12:53 by abaioumy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,9 +16,9 @@
     cd /var/www/html/wordpress/
 if ! wp core is-installed --allow-root; then
     wp core download --allow-root --force
-    wp config create --dbname=wordpress --dbuser=ayman --dbpass=vulpes19 --dbhost=mariadb --allow-root
-    wp core install --url=localhost --title=inception --admin_email=baioumyaymann@gmail.com --admin_user=abaioumy --admin_password=Baioumy123 --allow-root
-    wp user create guest guest@gmail.com --role=author --user_pass=guest --allow-root
+    wp config create --dbname=$(WORDPRESS_DB_NAME) --dbuser=$(WORDPRESS_DB_USER) --dbpass=$(WORDPRESS_DB_PASSWORD) --dbhost=$(WORDPRESS_DB_HOST) --allow-root
+    wp core install --url=$(WORDPRESS_URL) --title=$(WORDPRESS_TITLE) --admin_email=$(WORDPRESS_ADMIN_EMAIL) --admin_user=$(WORDPRESS_ADMIN_USER) --admin_password=$(WORDPRESS_ADMIN_PASSWORD) --allow-root
+    wp user create $(WORDPRESS_USER_NAME) $(WORDPRESS_USER_EMAIL) --role=author --user_pass=$(WORDPRESS_USER_PASSWORD) --allow-root
 else 
     echo "Wordpress already installed"
 fi
